916fde8701b97a64e1945ffe3286be7a8af34d71
saddam husain <saddamkhan.kahn705@gmail.com>
2022-01-18 18:45:50 +0530

    initial commit

---
+++ precache-manifest.e007fa28b8638a35d78d4c43eb36bf73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46a1545cbc92e3e126caf37f913aa92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64df393316e752fcd383",
+    "url": "/static/css/main.d118d360.chunk.css"
+  },
+  {
+    "revision": "24a0e0ba59888cde6855",
+    "url": "/static/js/2.3e64cd40.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3e64cd40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64df393316e752fcd383",
+    "url": "/static/js/main.e59ade7d.chunk.js"
+  },
+  {
+    "revision": "aa13cad0d31549ec41da",
+    "url": "/static/js/runtime-main.ae0ad31e.js"
+  }
+]);
